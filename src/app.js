f4b317ea4d0657dc034ed5fd15be0662c00bdc56
Abe-leiba <abel_con@yahoo.com>
2025-08-29 22:43:03 +0000

    pausing random cardgenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+ let suits = ['clubs', 'hearts', 'diamonds', 'spades'];
+ let cardval = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
+
+ 
+
+ 
+ console.log(suits[Math.floor(Math.random() * suits.length)])
+ console.log(cardval[Math.floor(Math.random() * cardval.length)])
+
   console.log("Hello Rigo from the console!");
 };
